7a1395d263fc8d2abb11cba80a8326ed3786916b
noahd615 <ndent@cristoreyny.org>
2017-02-02 13:19:17 +0000

    GO

---
+++ script.js
@@ -0,0 +1,13 @@
+var id = "AMSDVQOGOPNEAHV5Y4WS2YCIQWSGF14FRE2LZ0G2BVF5QFHD"
+
+var secret = "5CZPEAYSN5WDMJHFWV243PTMENLSF52WGAJEQDPRQFJLFB5G"
+
+ $(document).ready(function() {
+    $("#Find").click(function() {
+        var restaurant = sdocument.getElementById("search").value
+    
+        var apixu = https://api.foursquare.com/v2/venues/40a55d80f964a52020f31ee3
+        alert(apixu);
+        });
+    });
+
